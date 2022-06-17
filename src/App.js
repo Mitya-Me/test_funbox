7a20e4e51c6fac89e5fb12d7b73eec640f6512e7
mitya-me <dmitrymefodyev@gmail.com>
2022-06-17 21:48:35 +0300

    Start template (globalStyles, variables, fonts, favicon)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/globalStyles.module.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return <main>start template</main>
+};
 
 export default App;
